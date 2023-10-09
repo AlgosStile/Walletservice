b014d3c61c6c4ca4ea845e8533a3898057c2df36
AlgosStile <oleg.todor@mail.ru>
2023-10-09 09:37:36 +0300

    wrote code for 3 classes in repository

--- src/main/java/wallet_service/repository/PlayerRepository.java
+++ src/main/java/wallet_service/repository/PlayerRepository.java
@@ -1,4 +1,27 @@
 package wallet_service.repository;
 
+import wallet_service.model.Player;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class PlayerRepository {
+    private Map<String, Player> players;
+
+    public PlayerRepository() {
+        this.players = new ConcurrentHashMap<>();
+    }
+
+    public void addPlayer(Player player) {
+        players.put(player.getUsername(), player);
+    }
+
+    public Player getPlayer(String username) {
+        return players.get(username);
+    }
+
+    public Collection<Player> getAllPlayers() {
+        return players.values();
+    }
 }
+
--- src/main/java/wallet_service/repository/TransactionRepository.java
+++ src/main/java/wallet_service/repository/TransactionRepository.java
@@ -1,4 +1,26 @@
 package wallet_service.repository;
 
+import wallet_service.model.Transaction;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Collection;
+import java.util.Map;
+
 public class TransactionRepository {
+    private Map<String, Transaction> transactions;
+
+    public TransactionRepository() {
+        this.transactions = new ConcurrentHashMap<>();
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.put(transaction.getId(), transaction);
+    }
+
+    public Transaction getTransaction(String id) {
+        return transactions.get(id);
+    }
+
+    public Collection<Transaction> getAllTransactions() {
+        return transactions.values();
+    }
 }
