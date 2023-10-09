78d45fc1fa273e04dbbdce6b1ec0330d5dd96f82
AlgosStile <oleg.todor@mail.ru>
2023-10-09 10:09:55 +0300

    added javadoc for TransactionRepository

--- src/main/java/wallet_service/repository/TransactionRepository.java
+++ src/main/java/wallet_service/repository/TransactionRepository.java
@@ -5,21 +5,43 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * Класс, представляющий репозиторий транзакций.
+ */
 public class TransactionRepository {
-    private Map<String, Transaction> transactions;
+    private Map<String, Transaction> transactions; // Коллекция транзакций
 
+    /**
+     * Конструктор класса TransactionRepository.
+     */
     public TransactionRepository() {
         this.transactions = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Добавить транзакцию в репозиторий.
+     *
+     * @param transaction Транзакция для добавления
+     */
     public void addTransaction(Transaction transaction) {
         transactions.put(transaction.getId(), transaction);
     }
 
+    /**
+     * Получить транзакцию по идентификатору.
+     *
+     * @param id Идентификатор транзакции
+     * @return Транзакция с указанным идентификатором или null, если транзакция не найдена
+     */
     public Transaction getTransaction(String id) {
         return transactions.get(id);
     }
 
+    /**
+     * Получить все транзакции.
+     *
+     * @return Коллекция всех транзакций
+     */
     public Collection<Transaction> getAllTransactions() {
         return transactions.values();
     }
