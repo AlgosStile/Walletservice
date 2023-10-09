3bbf345b45872e7d36e09b17738aae59f443532a
AlgosStile <oleg.todor@mail.ru>
2023-10-09 10:05:57 +0300

    added javadoc for Player+Transaction

--- src/main/java/wallet_service/model/Player.java
+++ src/main/java/wallet_service/model/Player.java
@@ -4,12 +4,21 @@ import wallet_service.controller.TransactionType;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс, представляющий игрока.
+ */
 public class Player {
-    private String username;
-    private String password;
-    private double balance;
-    private List<Transaction> transactions;
+    private String username; // Имя пользователя
+    private String password; // Пароль
+    private double balance; // Баланс
+    private List<Transaction> transactions; // Список транзакций
 
+    /**
+     * Конструктор класса Player.
+     *
+     * @param username Имя пользователя
+     * @param password Пароль
+     */
     public Player(String username, String password) {
         this.username = username;
         this.password = password;
@@ -17,27 +26,58 @@ public class Player {
         this.transactions = new ArrayList<>();
     }
 
+    /**
+     * Получить имя пользователя.
+     *
+     * @return Имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Получить пароль.
+     *
+     * @return Пароль
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Получить баланс игрока.
+     *
+     * @return Баланс игрока
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Получить список транзакций игрока.
+     *
+     * @return Список транзакций игрока
+     */
     public List<Transaction> getTransactions() {
         return transactions;
     }
 
-
+    /**
+     * Добавить транзакцию в список транзакций игрока.
+     *
+     * @param transaction Транзакция для добавления
+     */
     public synchronized void addTransaction(Transaction transaction) {
         transactions.add(transaction);
     }
 
+    /**
+     * Выполнить дебетовую транзакцию.
+     *
+     * @param id     Идентификатор транзакции
+     * @param amount Сумма транзакции
+     * @throws Exception Если сумма отрицательная или недостаточно средств
+     */
     public void debit(String id, double amount) throws Exception {
         if (amount < 0) {
             throw new Exception("Сумма не может быть отрицательной");
@@ -51,14 +91,19 @@ public class Player {
         transactions.add(transaction);
     }
 
+    /**
+     * Выполнить кредитовую транзакцию.
+     *
+     * @param id     Идентификатор транзакции
+     * @param amount Сумма транзакции
+     * @throws Exception Если сумма отрицательная
+     */
     public void credit(String id, double amount) throws Exception {
-        if(amount < 0) {
+        if (amount < 0) {
             throw new Exception("Некорректная сумма");
         }
         this.balance += amount;
         Transaction transaction = new Transaction(id, amount, TransactionType.CREDIT);
         transactions.add(transaction);
     }
-
 }
-
--- src/main/java/wallet_service/model/Transaction.java
+++ src/main/java/wallet_service/model/Transaction.java
@@ -2,25 +2,50 @@ package wallet_service.model;
 
 import wallet_service.controller.TransactionType;
 
+/**
+ * Класс, представляющий транзакцию.
+ */
 public class Transaction {
-    private String id;
-    private double amount;
-    private TransactionType type;
+    private String id; // Идентификатор транзакции
+    private double amount; // Сумма транзакции
+    private TransactionType type; // Тип транзакции
 
+    /**
+     * Конструктор класса Transaction.
+     *
+     * @param id     Идентификатор транзакции
+     * @param amount Сумма транзакции
+     * @param type   Тип транзакции
+     */
     public Transaction(String id, double amount, TransactionType type) {
         this.id = id;
         this.amount = amount;
         this.type = type;
     }
 
+    /**
+     * Получить идентификатор транзакции.
+     *
+     * @return Идентификатор транзакции
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Получить сумму транзакции.
+     *
+     * @return Сумма транзакции
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     * Получить тип транзакции.
+     *
+     * @return Тип транзакции
+     */
     public TransactionType getType() {
         return type;
     }
