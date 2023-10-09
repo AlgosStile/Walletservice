5b9cb4d1be5237cdbfa95c1ee7f2076175de1461
AlgosStile <oleg.todor@mail.ru>
2023-10-09 10:02:37 +0300

    added javadoc for Action

--- src/main/java/wallet_service/model/Action.java
+++ src/main/java/wallet_service/model/Action.java
@@ -1,28 +1,50 @@
-
 package wallet_service.model;
 
+/**
+ * Класс, представляющий действие пользователя.
+ */
 public class Action {
-    private String username;
-    private String action;
-    private String detail;
+    private String username; // Имя пользователя
+    private String action; // Действие
+    private String detail; // Детали действия
 
+    /**
+     * Конструктор класса Action.
+     *
+     * @param username Имя пользователя
+     * @param action   Действие
+     * @param detail   Детали действия
+     */
     public Action(String username, String action, String detail) {
         this.username = username;
         this.action = action;
         this.detail = detail;
     }
 
+    /**
+     * Получить имя пользователя.
+     *
+     * @return Имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Получить действие.
+     *
+     * @return Действие
+     */
     public String getAction() {
         return action;
     }
 
+    /**
+     * Получить детали действия.
+     *
+     * @return Детали действия
+     */
     public String getDetail() {
         return detail;
     }
 }
-
-
