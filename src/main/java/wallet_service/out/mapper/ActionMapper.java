9036719d5bdc466da8060ed59b86a99d7d681af6
AlgosStile <oleg.todor@mail.ru>
2023-10-24 15:10:10 +0300

    added javadoc for mapper

--- src/main/java/wallet_service/out/mapper/ActionMapper.java
+++ src/main/java/wallet_service/out/mapper/ActionMapper.java
@@ -5,10 +5,23 @@ import org.mapstruct.factory.Mappers;
 import wallet_service.in.model.Action;
 import wallet_service.out.dto.ActionDto;
 
+/**
+ * Интерфейс-маппер для преобразования объектов Action в объекты ActionDto и наоборот.
+ *
+ * @author Олег Тодор
+ */
 @Mapper
 public interface ActionMapper {
     ActionMapper INSTANCE = Mappers.getMapper(ActionMapper.class);
 
+    /**
+     * Преобразует объект Action в объект ActionDto.
+     *
+     * @param action Объект Action, который требуется преобразовать.
+     * @return Объект ActionDto, полученный в результате преобразования.
+     */
     ActionDto actionToActionDto(Action action);
+
+
     Action actionDtoToAction(ActionDto actionDto);
 }
