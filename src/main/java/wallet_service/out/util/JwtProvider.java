074726b0e64c5e1b10bde6685c5bdc286dd792ff
AlgosStile <oleg.todor@mail.ru>
2023-10-24 15:20:22 +0300

    added javadoc for util

--- src/main/java/wallet_service/out/util/JwtProvider.java
+++ src/main/java/wallet_service/out/util/JwtProvider.java
@@ -9,11 +9,26 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+/**
+ * Класс JwtProvider используется для генерации и проверки JWT токенов.
+ * Данный класс испольует алгоритм HS512 для подписи токена.
+ * Секретный ключ и время жизни токена задаются внутри класса.
+ *
+ * @author Олег Тодор
+ */
 public class JwtProvider {
 
     private static final long TOKEN_EXPIRATION = 1;
     private static final String SECRET_KEY = "YourSecretKey";
 
+    /**
+     * Метод генерирует JWT токен для указанного пользователя.
+     * Используется алгоритм HS512 и секретный ключ для подписи токена.
+     * Время жизни токена составляет 1 час.
+     *
+     * @param username логин пользователя, для которого создается токен
+     * @return сгенерированный JWT токен
+     */
     public String generateToken(String username) {
         Date now = new Date();
         return Jwts.builder()
@@ -24,6 +39,15 @@ public class JwtProvider {
                 .compact();
     }
 
+    /**
+     * Метод проверяет валидность JWT токена, используя секретный ключ.
+     * В случае успешной проверки возвращается имя пользователя (Subject), указанное при создании токена.
+     * Если токен недействителен, выбрасывается RuntimeException.
+     *
+     * @param jwtToken токен, который необходимо проверить
+     * @return логин пользователя, которому принадлежит токен
+     * @throws RuntimeException если токен недействителен
+     */
     public String validateToken(String jwtToken) {
         try {
             Jws<Claims> claimsJws = Jwts.parserBuilder()
