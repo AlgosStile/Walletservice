76ea327f8707a1fea6edba9a3fbea151124f8c4c
AlgosStile <oleg.todor@mail.ru>
2023-10-24 15:00:46 +0300

    added javadoc for dto

--- src/main/java/wallet_service/out/dto/PlayerDto.java
+++ src/main/java/wallet_service/out/dto/PlayerDto.java
@@ -2,14 +2,32 @@ package wallet_service.out.dto;
 
 import jakarta.validation.constraints.*;
 
+/**
+ * Класс PlayerDto предназначен для передачи данных игрока в приложении.
+ * Он содержит информацию об имени пользователя, балансе и пароле игрока.
+ *
+ * @author Олег Тодор
+ */
 public class PlayerDto {
+
+    /**
+     * Имя пользователя. Должно быть не пустым.
+     */
     @NotBlank
     private String username;
 
+    /**
+     * Баланс игрока. Должен быть не нулевым и больше не 0.
+     */
     @NotNull
     @Min(0)
     private double balance;
 
+    /**
+     * Пароль пользователя. Должен содержать хотя бы одну цифру, одну строчную
+     * и одну прописную букву. Длина пароля должна быть от 6 до 15 символов.
+     * Пробелы не допускаются.
+     */
     @NotBlank
     @Size(min = 6, max = 15)
     @Pattern.List({
@@ -20,34 +38,63 @@ public class PlayerDto {
     })
     private String password;
 
+    /**
+     * Конструктор класса PlayerDto. Устанавливает имя пользователя и баланс,
+     * пароль установлен как пустая строка.
+     *
+     * @param username имя пользователя
+     * @param balance  баланс пользователя
+     */
     public PlayerDto(String username, double balance) {
         this.username = username;
         this.balance = balance;
         this.password = "";
     }
 
+    /**
+     * Пустой конструктор класса PlayerDto.
+     */
     public PlayerDto() {
 
     }
 
-
+    /**
+     * Возвращает имя пользователя.
+     * @return имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
-
+    /**
+     * Возвращает пароль пользователя.
+     * @return пароль пользователя
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Возвращает баланс пользователя.
+     * @return баланс пользователя
+     */
     public double getBalance() {
         return balance;
     }
 
-    public void setUsername(String test) {
-
+    /**
+     * Устанавливает новое имя пользователя.
+     * @param username новое имя пользователя
+     */
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setPassword(String test) {
+    /**
+     * Устанавливает новый пароль пользователя.
+     * @param password новый пароль пользователя
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- src/main/java/wallet_service/out/dto/TransactionDto.java
+++ src/main/java/wallet_service/out/dto/TransactionDto.java
@@ -8,21 +8,52 @@ import lombok.Getter;
 import wallet_service.in.controller.TransactionType;
 
 
+/**
+ * Класс TransactionDto представляет объект транзакции.
+ * Содержит информацию о ID транзакции, сумме, типе транзакции и имени пользователя.
+ * Используется для общения с пользователями и передачи данных.
+ *
+ * @author Олег Тодор
+ */
 @Getter
 public class TransactionDto {
+    /**
+     * Уникальный идентификатор транзакции.
+     * Не может быть пустым, это проверяется с помощью аннотации @NotBlank.
+     */
     @NotBlank(message = "Transaction id cannot be blank")
     private String id;
 
+    /**
+     * Сумма транзакции.
+     * Не может быть null и должна быть больше или равна 0, это проверяется с помощью аннотаций @NotNull и @Min.
+     */
     @NotNull(message = "Amount cannot be null")
     @Min(value = 0, message = "Amount should not be less than 0")
     private double amount;
 
+    /**
+     * Тип транзакции.
+     * Не может быть null, это проверяется с помощью @NotNull.
+     */
     @NotNull(message = "Transaction type cannot be null")
     private TransactionType type;
 
+    /**
+     * Имя пользователя, связанного с транзакцией.
+     * Не может быть пустым, это проверяется с помощью аннотации @NotBlank.
+     */
     @NotBlank(message = "Username cannot be blank")
     private String username;
 
+    /**
+     * Полный конструктор, инициализирующий все поля класса.
+     *
+     * @param id идентификатор транзакции.
+     * @param amount сумма транзакции.
+     * @param type тип транзакции.
+     * @param username имя пользователя.
+     */
     public TransactionDto(String id, double amount, TransactionType type, String username) {
         this.id = id;
         this.amount = amount;
@@ -30,25 +61,58 @@ public class TransactionDto {
         this.username = username;
     }
 
-    // По умолчанию публичный конструктор для Jackson
+    /**
+     * Конструктор по умолчанию.
+     * Необходим для правильной работы с фреймворком Jackson.
+     */
     public TransactionDto() {
     }
 
+    /**
+     * Конструктор инициализирует только часть полей класса: @param id, @param amount и @param type.
+     *
+     * @param id идентификатор транзакции.
+     * @param amount сумма транзакции.
+     * @param type тип транзакции.
+     */
     public TransactionDto(String id, double amount, TransactionType type) {
+        this.id = id;
+        this.amount = amount;
+        this.type = type;
     }
 
+    /**
+     * Устанавливает идентификатор транзакции.
+     *
+     * @param id идентификатор транзакции.
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Устанавливает сумму транзакции.
+     *
+     * @param amount сумма транзакции.
+     */
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * Устанавливает тип транзакции.
+     *
+     * @param type тип транзакции.
+     */
     public void setType(TransactionType type) {
         this.type = type;
     }
 
+    /**
+     * Устанавливает имя пользователя, связанного с транзакцией.
+     *
+     * @param username имя пользователя.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
