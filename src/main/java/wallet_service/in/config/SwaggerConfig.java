f8fe7826cb1a7c063754b6289e0813797e436b1f
AlgosStile <oleg.todor@mail.ru>
2023-10-30 16:44:34 +0300

    added javadoc for swagger

--- src/main/java/wallet_service/in/config/SwaggerConfig.java
+++ src/main/java/wallet_service/in/config/SwaggerConfig.java
@@ -12,17 +12,28 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+/**
+ * Класс SwaggerConfig является конфигурационным классом для настройки Swagger в приложении Wallet Service.
+ * Он используется для создания и настройки объекта Docket, который предоставляет конфигурацию для Swagger.
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     private final String configFile;
-
+    /**
+     * Конструктор класса SwaggerConfig.
+     *
+     * @param configFile путь к конфигурационному файлу Swagger UI.
+     */
     public SwaggerConfig(@Value("${springfox.documentation.swagger-ui.config-file}") String configFile) {
         this.configFile = configFile;
     }
-
+    /**
+     * Метод для создания и настройки объекта Docket, предоставляющего конфигурацию для Swagger.
+     *
+     * @return объект Docket с настроенными параметрами Swagger.
+     */
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -33,7 +44,11 @@ public class SwaggerConfig {
                 .enableUrlTemplating(false)
                 .enable(configFile != null && !configFile.isEmpty());
     }
-
+    /**
+     * Метод для создания объекта ApiInfo, предоставляющего информацию о API.
+     *
+     * @return объект ApiInfo с информацией о заголовке, описании и версии API.
+     */
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("WalletServiceApplication").description("WalletServiceApplication - Wallet Service").version("1.0.0").build();
