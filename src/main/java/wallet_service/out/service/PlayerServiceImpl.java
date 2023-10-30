acdbef2d4234fbb06a44a40500a96a9afa146a99
AlgosStile <oleg.todor@mail.ru>
2023-10-30 17:23:16 +0300

    added javadoc for service

--- src/main/java/wallet_service/out/service/PlayerServiceImpl.java
+++ src/main/java/wallet_service/out/service/PlayerServiceImpl.java
@@ -11,12 +11,23 @@ import wallet_service.out.repository.TransactionRepository;
 import java.math.BigDecimal;
 import java.util.List;
 
+
+/**
+ * Сервис для работы с сущностью Player.
+ */
 @Service
 public class PlayerServiceImpl {
     private final PlayerRepository playerRepository;
     private final ActionRepository actionRepository;
     private final TransactionRepository transactionRepository;
 
+    /**
+     * Конструктор класса.
+     *
+     * @param playerRepository      репозиторий для работы с сущностью Player
+     * @param actionRepository      репозиторий для работы с сущностью Action
+     * @param transactionRepository репозиторий для работы с сущностью Transaction
+     */
     public PlayerServiceImpl(PlayerRepository playerRepository, ActionRepository actionRepository, TransactionRepository transactionRepository) {
         this.playerRepository = playerRepository;
         this.actionRepository = actionRepository;
@@ -24,14 +35,33 @@ public class PlayerServiceImpl {
     }
 
 
+    /**
+     * Возвращает игрока с указанным именем пользователя.
+     *
+     * @param username имя пользователя
+     * @return игрок
+     */
     public Player getPlayer(String username) {
         return playerRepository.findByUsername(username);
     }
 
+
+    /**
+     * Сохраняет игрока.
+     *
+     * @param player игрок
+     */
     public void savePlayer(Player player) {
         playerRepository.save(player);
     }
 
+
+    /**
+     * Списывает средства с баланса игрока с указанным именем пользователя.
+     *
+     * @param username имя пользователя
+     * @param amount   сумма списания
+     */
     public void debitTransaction(String username, BigDecimal amount) {
         Player player = playerRepository.findByUsername(username);
         BigDecimal newBalance = BigDecimal.valueOf(player.getBalance()).subtract(amount);
@@ -39,6 +69,13 @@ public class PlayerServiceImpl {
         playerRepository.save(player);
     }
 
+
+    /**
+     * Зачисляет средства на баланс игрока с указанным именем пользователя.
+     *
+     * @param username имя пользователя
+     * @param amount   сумма зачисления
+     */
     public void creditTransaction(String username, BigDecimal amount) {
         Player player = playerRepository.findByUsername(username);
         BigDecimal newBalance = BigDecimal.valueOf(player.getBalance()).add(amount);
@@ -46,9 +83,18 @@ public class PlayerServiceImpl {
         playerRepository.save(player);
     }
 
+
+    /**
+     * Возвращает список действий игрока с указанным именем пользователя.
+     *
+     * @param username имя пользователя
+     * @return список действий
+     */
     public List<Action> getPlayerActions(String username) {
         return actionRepository.findByUsername(username);
     }
+
+
 
     public void debit(String username, int id, double amount) {
     }
