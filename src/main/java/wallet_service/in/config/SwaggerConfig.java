d56d1dac9b1c87663f3654fe9ed5846abf3536ba
AlgosStile <oleg.todor@mail.ru>
2023-10-30 13:27:28 +0300

    add type reduction

--- src/main/java/wallet_service/in/config/SwaggerConfig.java
+++ src/main/java/wallet_service/in/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package wallet_service.in.config;
 
+import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,18 +12,25 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-
 @Configuration
 public class SwaggerConfig {
+
+    private final String configFile;
+
+    public SwaggerConfig(@Value("${springfox.documentation.swagger-ui.config-file}") String configFile) {
+        this.configFile = configFile;
+    }
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("wallet_service"))
-                .paths(PathSelectors.any())
+                .apis((Predicate<RequestHandler>) RequestHandlerSelectors.basePackage("wallet_service.out.controller"))
+                .paths((Predicate<String>) PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .enableUrlTemplating(false)
+                .enable(configFile != null && !configFile.isEmpty());
     }
 
     private ApiInfo apiInfo() {
