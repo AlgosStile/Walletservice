2da4bd1d64516f1559c54e6775dd285cd09d22d9
AlgosStile <oleg.todor@mail.ru>
2023-11-03 16:16:11 +0300

    tapped the code into the walletservice

--- src/main/java/wallet_service/WalletServiceApplication.java
+++ src/main/java/wallet_service/WalletServiceApplication.java
@@ -1,16 +1,11 @@
 package wallet_service;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import wallet_service.in.config.Config;
-import wallet_service.in.config.SwaggerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class WalletServiceApplication {
-
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("wallet_service.in.config", "wallet_service.out.service", "wallet_service.out.repository", "wallet_service.out.aspect", "wallet_service.out.controller", "wallet_service.out.model");
-        context.register(Config.class, SwaggerConfig.class);
-        context.refresh();
-        context.close();
+        SpringApplication.run(WalletServiceApplication.class, args);
     }
 }
