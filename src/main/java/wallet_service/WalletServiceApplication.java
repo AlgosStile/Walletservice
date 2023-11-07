c5778958006e399d402c3b9e8e0c34b279dbdfbf
AlgosStile <oleg.todor@mail.ru>
2023-11-07 19:30:45 +0300

    added javadoc main

--- src/main/java/wallet_service/WalletServiceApplication.java
+++ src/main/java/wallet_service/WalletServiceApplication.java
@@ -5,11 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
+/**
+ * Класс WalletServiceApplication представляет главный класс приложения кошелька,
+ * который использует фреймворк Spring Boot.
+ *
+ * @Author Олег Тодор
+ */
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class WalletServiceApplication {
+    /**
+     * Главный метод, запускающий приложение кошелька.
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         SpringApplication.run(WalletServiceApplication.class, args);
     }
 }
+
 
