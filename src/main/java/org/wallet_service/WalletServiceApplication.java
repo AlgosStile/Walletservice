7b7a89d3e3c07cef0c998c679f9fd0e859a87766
AlgosStile <oleg.todor@mail.ru>
2023-11-08 15:53:40 +0300

    javadoc main

--- src/main/java/org/wallet_service/WalletServiceApplication.java
+++ src/main/java/org/wallet_service/WalletServiceApplication.java
@@ -4,10 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * Входная точка для приложения основанного на Spring Boot, называемого "WalletService".
+ * Этот сервис является частью пакета main.java.org.wallet_service.
+ *
+ * @author Олег Тодор
+ * @version 5.0
+ */
 @SpringBootApplication
 public class WalletServiceApplication {
+    /**
+     * Основной метод для запуска приложения.
+     *
+     * @param args аргументы командной строки, которые можно передать при запуске
+     * приложения.
+     */
     public static void main(String[] args) {
         SpringApplication.run(WalletServiceApplication.class, args);
     }
 }
+
 
